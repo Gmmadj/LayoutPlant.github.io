35ef347560464cfd22fa4d7b4ac1c3590569f565
Gmmadj <56927849+Gmmadj@users.noreply.github.com>
2020-08-12 20:47:27 +0300

    New File

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+  $('.slaider-plants').slick({
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2
+        }
+      }
+    ]
+  });
+  $('.slaider-plants-2').slick({
+    prevArrow: $('.prev-2'),
+    nextArrow: $('.next-2'),
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2
+        }
+      }
+    ],
+  });
+  $('.slaider-news').slick({
+    prevArrow: $('.prev-news'),
+    nextArrow: $('.next-news'),
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    variableWidth: true,
+  });
+});
